6fad02f7e343bb2a4eac35e200098403db4c4af0
thienphucsssssss <phamphucthien2325@gmail.com>
2026-08-03 16:26:52 +0700

    fix: allow unsplash domain in next.config for images

--- next.config.ts
+++ next.config.ts
@@ -13,13 +13,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
       },
